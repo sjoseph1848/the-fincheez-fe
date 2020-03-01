73e95dafc392efbd890a735ef1c2fcb00a01961e
sjoseph1848 <sjoseph1848@gmail.com>
2020-03-01 14:10:06 -0600

    added all the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,56 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddStockComponent } from './components/add-stock/add-stock.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EditStockComponent } from './components/edit-stock/edit-stock.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { StockDetailsComponent } from './components/stock-details/stock-details.component';
+import { StocksComponent } from './components/stocks/stocks.component';
+
+// flash messages
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+// Angular fire
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// Services
+import { StockService } from './services/stock.service';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStockComponent,
+    DashboardComponent,
+    EditStockComponent,
+    LoginComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    RegisterComponent,
+    SettingsComponent,
+    SidebarComponent,
+    StockDetailsComponent,
+    StocksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, 'fincheez'),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FlashMessagesModule.forRoot(),
   ],
-  providers: [],
+  providers: [StockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA4dXXH5t2wXGqbCIEA-PQ4m6iM9uH1BPU",
+    authDomain: "fincheez-55527.firebaseapp.com",
+    databaseURL: "https://fincheez-55527.firebaseio.com",
+    projectId: "fincheez-55527",
+    storageBucket: "fincheez-55527.appspot.com",
+    messagingSenderId: "1078523568432",
+    appId: "1:1078523568432:web:3f376c82ee0e74bb6ba8f5",
+    measurementId: "G-VXG7JQBZJN"
+  }
 };
 
 /*
