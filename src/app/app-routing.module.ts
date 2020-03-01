c9dcec6062b3f2e7d2b3c54f062585980f7c34df
sjoseph1848 <sjoseph1848@gmail.com>
2020-03-01 14:13:12 -0600

    added model and service for stock

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,45 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Import the components
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AddStockComponent } from './components/add-stock/add-stock.component';
+import { EditStockComponent } from './components/edit-stock/edit-stock.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { StockDetailsComponent } from './components/stock-details/stock-details.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '', component: DashboardComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'stock/add', component: AddStockComponent
+  },
+  {
+    path: 'stock/edit/:id', component: EditStockComponent
+  },
+  {
+    path: 'stock/:id', component: StockDetailsComponent
+  },
+  {
+    path: 'settings', component: SettingsComponent
+  },
+  {
+    path: '**', component: NotFoundComponent
+  }
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from '@angular/fire/firestore';
+import 'firebase/firestore';
+import { Stock } from '../models/Stock';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockService {
 
-  constructor() { }
+  stocksCollection: AngularFirestoreCollection<Stock>;
+  stockDoc: AngularFirestoreDocument<Stock>;
+  stocks: Observable<Stock[]>;
+  stock: Observable<Stock>;
+
+  constructor(private afs: AngularFirestore) {
+    this.stocksCollection = this.afs.collection('stocks', ref =>
+      ref.orderBy('symbol', 'asc')
+    );
+  }
+
+  getStocks(): Observable<Stock[]> {
+    // get clients with the id
+    this.stocks = this.stocksCollection.snapshotChanges().pipe(
+      map(changes => {
+        return changes.map(action => {
+          const data = action.payload.doc.data() as Stock;
+          data.id = action.payload.doc.id;
+          return data;
+        });
+      })
+    );
+
+    return this.stocks;
+  }
+
+  newStock(client: Stock) {
+    this.stocksCollection.add(client);
+  }
 }
